8c5ec92dc0f3535ce2580c533da3a526595d65ca
MaquinoBSC <martinare.ipn@gmail.com>
2023-03-04 00:29:07 -0600

    Se crea aplicación y se realiza limpieza de codigo

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hola Maquino, one more time in React Native</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
